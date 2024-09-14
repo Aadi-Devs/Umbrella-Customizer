02d436bcf33c9dcd0be79dd4a43a4aa02e42b320
Aditya Shrivastav <devs.aadi20@gmail.com>
2024-09-15 02:16:30 +0530

    4th commit

--- umbrella.js
+++ umbrella.js
@@ -1,4 +1,3 @@
-// Selecting all the required elements
 const imgSection = document.querySelector(".image-section .umbrella-image");
 const svgLoader = document.querySelector(".image-section .svg-loader");
 const colorButtons = document.querySelectorAll(".color-btn");
@@ -6,38 +5,53 @@ const uploadBtn = document.querySelector(".upload-btn");
 const fileInput = document.querySelector(".logo-upload");
 const logoPreview = document.querySelector(".logo-preview");
 const closeBtn = document.querySelector(".close-btn");
+const uploadIcon = document.querySelector(".upload-icon");
 const body = document.querySelector("body");
 
 colorButtons.forEach((button) => {
   button.addEventListener("click", () => {
     const color = button.dataset.color;
+    let fillColor;
 
+    //Code to Show the loader immediately
     svgLoader.style.display = "block";
     imgSection.classList.add("hidden");
     logoPreview.classList.add("hidden");
 
+    // Update the color of the background and the upload button
+    switch (color) {
+      case "Pink":
+        fillColor = "#D41B7E";
+        uploadBtn.style.backgroundColor = fillColor;
+        body.style.backgroundColor = `${fillColor}38`;
+        break;
+      case "Blue":
+        fillColor = "#14AAE2";
+        uploadBtn.style.backgroundColor = fillColor;
+        body.style.backgroundColor = `${fillColor}38`;
+        break;
+      case "Yellow":
+        fillColor = "#F4C93E";
+        uploadBtn.style.backgroundColor = fillColor;
+        body.style.backgroundColor = `${fillColor}2E`;
+        break;
+      default:
+        fillColor = "#000000";
+        uploadBtn.style.backgroundColor = fillColor;
+        body.style.backgroundColor = "#0000001A";
+        break;
+    }
+
+    // Set the fill color of the SVG path
+    svgLoader.querySelector("path").setAttribute("fill", fillColor);
+
+    // Hiding the loader and updating the image
     setTimeout(() => {
       svgLoader.style.display = "none";
       imgSection.setAttribute("src", `./assets/${color} umbrella.png`);
       imgSection.classList.remove("hidden");
       logoPreview.classList.remove("hidden");
-
-      // Code for changing the colours of the background and the upload button. 
-
-      if (color === "Pink") {
-        uploadBtn.style.transition = "background-color 0.3s ease";
-        uploadBtn.style.backgroundColor = "#D41B7E";
-        body.style.backgroundColor = "#d41b7e38";
-      } else if (color === "Blue") {
-        uploadBtn.style.transition = "background-color 0.3s ease";
-        uploadBtn.style.backgroundColor = "#14AAE2";
-        body.style.backgroundColor = "#14abe238";
-      } else {
-        uploadBtn.style.transition = "background-color 0.3s ease";
-        uploadBtn.style.backgroundColor = "#F4C93E";
-        body.style.backgroundColor = "#f4ca3e2e";
-      }
-    }, 500); // delay in changing the colours of the background and the upload button 
+    }, 500); // time taken to wait for hiding the loader
   });
 });
 
@@ -51,19 +65,23 @@ fileInput.addEventListener("change", (event) => {
   if (file) {
     const reader = new FileReader();
 
+    uploadIcon.src = "./assets/loader_icon.svg";
+    uploadIcon.classList.add("rotating");
+
     reader.onload = (e) => {
-      // This code is to show the loder icon and hide the other elements
       svgLoader.style.display = "block";
       logoPreview.style.display = "none";
       imgSection.style.display = "none";
 
-      // this code is for waitinng for a few seconds before showing the logo and hiding the loader
       setTimeout(() => {
         svgLoader.style.display = "none";
         logoPreview.src = e.target.result;
         logoPreview.style.display = "block";
         imgSection.style.display = "block";
-      }, 1000); // we have to adjust this for our desired delay.
+
+        uploadIcon.src = "./assets/upload_icon.svg";
+        uploadIcon.classList.remove("rotating");
+      }, 1000);
     };
 
     reader.readAsDataURL(file);
